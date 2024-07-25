28237412fade9c6db70dd2d7f9840e9cdc79b508
Bruno Léo Silva <brunoleosilva@hotmail.com>
2024-07-24 22:55:49 -0300

    ajustes activity

--- app/src/main/java/com/example/professorallocation/ui/DepartmentActivity.kt
+++ app/src/main/java/com/example/professorallocation/ui/DepartmentActivity.kt
@@ -1,9 +1,14 @@
 package com.example.professorallocation.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
 import com.example.professorallocation.R
+import com.example.professorallocation.repository.DepartmentRepository
+import com.example.professorallocation.repository.RetrofitConfig
+import com.example.professorallocation.utils.CustomAdapter
 
 class DepartmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +19,22 @@ class DepartmentActivity : AppCompatActivity() {
         mainToolbar.title = ""
         setSupportActionBar(mainToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val departmentRepository = DepartmentRepository(RetrofitConfig.departmentService)
+
+        getDepartments(departmentRepository)
+    }
+
+    private fun getDepartments(repository: DepartmentRepository) {
+        return repository.getDepartments(
+            onCall = { departments ->
+                Log.i(">>>", "success get departments")
+                val rvList = findViewById<RecyclerView>(R.id.rvDepartmentList)
+                rvList.adapter = departments?.map { it.name }?.let { CustomAdapter(it) }
+            },
+            onError = {
+                Log.e(">>>", "error get departments $it")
+            }
+        )
     }
 }
--- app/src/main/java/com/example/professorallocation/ui/ProfessorActivity.kt
+++ app/src/main/java/com/example/professorallocation/ui/ProfessorActivity.kt
@@ -1,9 +1,14 @@
 package com.example.professorallocation.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
 import com.example.professorallocation.R
+import com.example.professorallocation.repository.ProfessorRepository
+import com.example.professorallocation.repository.RetrofitConfig
+import com.example.professorallocation.utils.CustomAdapter
 
 class ProfessorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +19,22 @@ class ProfessorActivity : AppCompatActivity() {
         mainToolbar.title = ""
         setSupportActionBar(mainToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val professorRepository = ProfessorRepository(RetrofitConfig.professorService)
+
+        getProfessors(professorRepository)
+    }
+
+    private fun getProfessors(repository: ProfessorRepository) {
+        return repository.getProfessors(
+            onCall = { professors ->
+                Log.i(">>>", "success get professors")
+                val rvList = findViewById<RecyclerView>(R.id.rvProfessorsList)
+                rvList.adapter = professors?.map { it.name }?.let { CustomAdapter(it) }
+            },
+            onError = {
+                Log.e(">>>", "error get professors $it")
+            }
+        )
     }
 }
